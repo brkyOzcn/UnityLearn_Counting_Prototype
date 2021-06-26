f6950ce56a01055c59b0f7459a62c6db1eef4ee4
Berkay Özcan <berkayozcnn@outlook.com>
2021-06-26 21:59:35 +0300

    ....

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float speed;
+    private float horizontalInput;
+    private float verticalInput;
+
     void Start()
     {
+
+    }
+
+    
+    void Update()
+    {
+        CharacterMovement(speed);
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            CharacterMovement(10.0f);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void CharacterMovement(float speed)
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.left * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.back * verticalInput * speed * Time.deltaTime);
     }
+
 }
